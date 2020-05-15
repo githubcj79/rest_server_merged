c1d069759045dea28c90a143b8b960b1ec2a06b3
ghost <carlosjmnz79@gmail.com>
2020-05-15 15:48:13 -0400

    First route added

--- rest-server.py
+++ rest-server.py
@@ -1,6 +1,27 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
+
+tasks = [
+    {
+        'id': 1,
+        'title': u'Buy groceries',
+        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol',
+        'done': False
+    },
+    {
+        'id': 2,
+        'title': u'Learn Python',
+        'description': u'Need to find a good Python tutorial on the web',
+        'done': False
+    }
+]
+
+# CHROME: http://127.0.0.1:5000/people
+# GET /people -> devuelve la lista completa de las personas existentes
+@app.route('/people', methods=['GET'])
+def get_tasks():
+    return jsonify({'tasks': tasks})
 
 @app.route('/')
 def index():
